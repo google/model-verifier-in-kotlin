03f6c7e71e3ede72db21d7e235bb96944fa76d25
Matti Peltomaki <mpeltomaki@google.com>
2023-07-16 10:32:08 -0700

    document ModelClassSubject

--- testing/ModelClassSubject.kt
+++ testing/ModelClassSubject.kt
@@ -20,6 +20,21 @@ import com.google.common.truth.Truth
 import com.google.mvik.framework.Model
 import com.google.mvik.framework.Result
 
+/**
+ * Assertions for model classes.
+ * 
+ * The basic usage is: assertThat(MyModel()).succeeds() or assertThat(MyModel()).fails().
+ * This solves the model and asserts that the result is respectively either a success or
+ * a failure.
+ * 
+ * By default, the initial state from the model definition is used. It's sometimes useful
+ * to use a different initial state, which can be provides with
+ * 
+ * assertThat(MyModel()).startingFrom(initialState).succeeds().
+ *
+ * More precise flavors of test can be written using failsWith() or reaches(). See the
+ * docs on ModelClassSubject for more details.
+ */
 fun <State : Any> assertThat(modelClass: Model<State>): ModelClassSubject<State> {
   return ModelClassSubject(modelClass)
 }
@@ -29,26 +44,52 @@ class ModelClassSubject<State : Any>(val model: Model<State>, val initState: Sta
     return ModelClassSubject(model, initState)
   }
 
+  /**
+   * Asserts that the model succeeds.
+   */
   fun succeeds() {
     val result = solve()
     if (!result.success) throw AssertionError("failed in $result.endState")
   }
 
+  /**
+   * Asserts that the model fails.
+   */
   fun fails() {
     Truth.assertThat(solve().success).isFalse()
   }
 
+  /**
+   * Asserts that the model fails ending up in the provided state.
+   * 
+   * This is only useful in narrow scenarios. The order in which the state space
+   * is searched is undefined and can be non-deterministic. Models that have multiple
+   * failing states aren't guaranteed to hit a specific one.
+   */
   fun failsWith(target: State) {
     val result = solve()
     Truth.assertThat(result.success).isFalse()
     Truth.assertThat(result.endState).isEqualTo(target)
   }
 
+  /**
+   * Asserts that the model reaches the specified state.
+   * 
+   * This primarily intended for unit testing that steps produce the intended outcome.
+   */
   fun reaches(target: State) {
     val safetyFailingAtTarget = Model.SafetySpec({ s: State -> s != target })
     Truth.assertThat(solve(listOf(safetyFailingAtTarget)).success).isFalse()
   }
 
+  /**
+   * Asserts that the model reaches any state satisfying the predicate.
+   * 
+   * This primarily intended for unit testing that steps produce the intended outcome.
+   * 
+   * This overload can be used to avoid specifying the full target state in cases
+   * the states are large and fully writing them out is cumbersome.
+   */
   fun reaches(pred: (State) -> Boolean) {
     val safetyFailingAtTarget = Model.SafetySpec({ s: State -> !pred(s) })
     Truth.assertThat(solve(listOf(safetyFailingAtTarget)).success).isFalse()
